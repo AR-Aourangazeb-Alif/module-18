7896c2e5ffc02928181c2a813972506fd7db429c
AR Aourangazeb Alif <ar.aourangazeb.alif@gmail.com>
2024-02-01 21:22:24 +0600

    task 2

---
+++ task.js
@@ -0,0 +1,22 @@
+// break task 1 
+// for(let i = 1; i <= 200; i++){
+//     console.log(i);
+
+//     if(i===100){
+//         break;
+//     }
+// }
+
+
+// break task 2 
+// let i = 1;
+// let sum = 0;
+
+// while(true){
+//     sum = sum + i;
+//     if(sum >= 100){
+//         break;
+//     }
+//     i++;
+// }
+// console.log(sum);
